8d9b26ffb14f910ac8d0571b00417ed806f96637
Jay Gandhi Github <jaygandhi249@gmail.com>
2023-04-10 19:52:19 +1000

    Initial Check-in of the FreshVotes code

---
+++ FreshVote/src/test/java/com/freshVote/FreshVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshVote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
